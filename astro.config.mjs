8aa8d323d20992e74ffd95d11800790fe6417e07
MToyokura <mikitoyo6@gmail.com>
2026-04-21 23:12:05 +0900

    add deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mtoyokura.github.io",
+  base: "/widgets",
   integrations: [
     svelte(),
     starlight({
